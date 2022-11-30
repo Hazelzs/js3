a834da9e55721cf500b5fafc29a3ee8630d084f4
Hazelzs <92879328+Hazelzs@users.noreply.github.com>
2022-11-30 14:07:18 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(email, name, age){
+        this.email = email;
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let user1 = new User('asda@gmail.com', 'asda', '31');
+let user2 = new User('asda1@gmail.com', 'ben', '24');
+let user3 = new User('asda2@gmail.com', 'sammy', '26');
+let user4 = new User('asda2@gmail.com', 'ari', '27');
+let user5 = new User('asda3@gmail.com', 'emily', '28');
+let user6 = new User('asda4@gmail.com', 'grace', '45');
+let user7 = new User('asda5@gmail.com', 'landon', '18');
+let user8 = new User('asda6@gmail.com', 'josh', '22');
+let user9 = new User('asda7@gmail.com', 'will', '61');
+let user10 = new User('asda9@gmail.com', 'shawn', '32');
