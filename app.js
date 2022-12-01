6aebc8c8fdd6e5126b1a9c86b9b69a28fddceafb
Hazelzs <92879328+Hazelzs@users.noreply.github.com>
2022-12-01 14:31:22 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-class User {
-    constructor(email, name, age){
-        this.email = email;
-        this.name = name;
-        this.age = age;
+let firstName = ``;
+let lastName = ``;
+let age = ``;
+let city = ``;
+function userI() {
+    firstName = prompt(`What is your first name?`);
+    lastName = prompt(`What is your last name?`);
+    age = prompt(`What is your age?`);
+    city = prompt(`What city do you live in?`);
+    return(firstName, lastName, age, city)
+}
+function newUser() {
+    let i = 1;
+    let number = 1;
+    while (i <= 4) {
+        userI();
+        class user {
+            constructor(firstName, lastName, age, city) {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.age = age;
+                this.city = city;
+            }
+        } 
+        console.log(`user${number}, First Name: ${firstName}, Last Name: ${lastName}, Age: ${age}, City of Residence: ${city}`);
+        number++;
+        i++;
     }
 }
-
-let user1 = new User('asda@gmail.com', 'asda', '31');
-let user2 = new User('asda1@gmail.com', 'ben', '24');
-let user3 = new User('asda2@gmail.com', 'sammy', '26');
-let user4 = new User('asda2@gmail.com', 'ari', '27');
-let user5 = new User('asda3@gmail.com', 'emily', '28');
-let user6 = new User('asda4@gmail.com', 'grace', '45');
-let user7 = new User('asda5@gmail.com', 'landon', '18');
-let user8 = new User('asda6@gmail.com', 'josh', '22');
-let user9 = new User('asda7@gmail.com', 'will', '61');
-let user10 = new User('asda9@gmail.com', 'shawn', '32');
+newUser()
